610f0e319c6552a67e18727ee843e90abcd3f058
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-22 12:05:42 +0300

    we got a database up

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,4 @@
+-- this scripts creates a table for users with unique email addresses
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
