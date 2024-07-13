8dfbf0774c93756ff8705d4d3b8521fff41b7d4f
Abraham John <jabrahamjohns@gmail.com>
2024-07-13 18:37:37 +0300

    fix: Old School band: script listing all bands with Glam rock as their main style  ranked by their lengevity

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,17 +1,8 @@
--- Create a temporary table to store the computed lifespan
-CREATE TEMPORARY TABLE temp_bands (
-    band_name VARCHAR(255),
-    lifespan INT
-);
+-- Lists all bands with Glam rock as their style,
+-- ranked by their longevity
+-- Column names must be: band_name & lifespan
 
--- Insert data into the temporary table by computing the lifespan
-INSERT INTO temp_bands (band_name, lifespan)
-SELECT band_name, 
-       (YEAR('2022-01-01') - CAST(SUBSTRING_INDEX(lifespan, '-', 1) AS UNSIGNED)) AS lifespan
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%';
-
--- Retrieve the bands ranked by their longevity
-SELECT band_name, lifespan
-FROM temp_bands
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
