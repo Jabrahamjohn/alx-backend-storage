4912598e2d0cfee548b9c370cc60af5a779a1013
Abraham John <jabrahamjohns@gmail.com>
2024-07-13 18:59:06 +0300

    fix: average score: creates a stored procedure to compute and store the average weighted score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,14 @@
---- Query: Create a stored procedure named ComputeAverageScoreForUser that receives a user_id as a parameter and calculates the average score for that user. The result should be stored in the average_scores table. The average score should be stored with two decimal places.
-DELIMITER //
+-- Creates a stored procedure ComputeAverageScoreForUser
+-- that computes and stores the average score for a student
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
-BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
-
-    SELECT AVG(score) INTO avg_score
-    FROM scores
-    WHERE user_id = user_id;
-
-    INSERT INTO average_scores (user_id, average_score)
-    VALUES (user_id, avg_score);
-END //
-
-DELIMITER ;
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(IN `user_id` INT)
+BEGIN 
+    UPDATE users
+    SET average_score = (SELECT AVG(score)
+                        FROM corrections
+                        WHERE corrections.user_id = user_id)
+    WHERE id = user_id;
+END $$
+DELIMITER ;$$
