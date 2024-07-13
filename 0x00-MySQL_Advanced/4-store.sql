bfe285e6099a569626584216f1cfac5bcd2c8203
Abraham John <jabrahamjohns@gmail.com>
2024-07-13 18:41:08 +0300

    fix: buy buy buy: creates trigger that decreases the quantity of an item after adding a new order

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,12 +1,11 @@
--- creates trigger that decrease quantity of items in stock after order is placed
-DELIMITER //
+-- Creates a trigger that decreases the quantity
+-- of an item after adding a new order
+-- New & OLD are MySQL extensions to triggers
+-- enable to access columns in the rows affected by a trigger
 
-CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE id = NEW.item_id;
-END //
-
-DELIMITER ;
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
