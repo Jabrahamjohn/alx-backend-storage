492e38bef03c4a3b64d7037b0affd29d3713d7d0
Abraham John <jabrahamjohns@gmail.com>
2024-07-13 18:52:34 +0300

    fix: add bonus file: script that creates a stored product and adds new correction for a student

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,21 +1,20 @@
---- creates a stored procedure
-DELIMITER //
+-- Creates a stored procedure AddBonus
+-- that adds a new correction for a student.
 
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+DELIMITER $$
+DROP PROCEDURE IF EXISTS AddBonus;
+CREATE PROCEDURE AddBonus(
+	IN `user_id` INTEGER,
+	IN `project_name` VARCHAR(255),
+	IN `score` INTEGER
+)
 BEGIN
-    DECLARE project_id INT;
+    INSERT INTO projects (name)
+    SELECT project_name
+    -- WHERE NOT EXISTS (SELECT * FROM projects WHERE name=project_name LIMIT 1);
+    WHERE project_name NOT IN (SELECT name FROM projects);
 
-    -- Check if project_name exists in projects table
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
-
-    -- If project_name does not exist, create a new project
-    IF project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (project_name);
-        SET project_id = LAST_INSERT_ID();
-    END IF;
-
-    -- Insert the new correction for the student
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-END //
-
-DELIMITER ;
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES(user_id, (SELECT id from projects WHERE name=project_name), score);
+END $$
+DELIMITER ;$$
