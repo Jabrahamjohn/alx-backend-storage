04744e162f9e1d1359d195556cba2019998e248e
Abraham John <jabrahamjohns@gmail.com>
2024-07-13 19:03:38 +0300

    fix: optimize search and score: creates an index on the table and the first letter and the score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,7 +1,5 @@
--- Import the table dump
--- Assuming the table dump file is names.sql
--- Replace 'path/to/names.sql' with the actual path to the names.sql file
-SOURCE path/to/names.sql;
+-- Creates an index idx_name_first_score on the table
+-- names and the first letter of name and the score
 
--- Create the index
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), LEFT(score, 1));
+CREATE INDEX idx_name_first_score
+ON names(name(1), score);
