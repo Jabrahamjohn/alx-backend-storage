7f9fdfc26dd0ce0f0c6cfc45ebed1c6f2604be3b
Abraham John <jabrahamjohns@gmail.com>
2024-07-13 18:34:40 +0300

    fix: Best band ever! : sql script that ranks country origins of bands ordered by the number of non unique fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,12 +1,8 @@
--- Create a temporary table to store the aggregated fan counts per country
--- Create a temporary table to store the rankings
-CREATE TEMPORARY TABLE temp_rankings AS
-SELECT origin, COUNT(*) AS nb_fans
-FROM metal_bands
-GROUP BY origin;
+-- Ranks country origins of bands
+-- Ordered by the number of (non-unique) fans
+-- Column names must be: origin and nb_fans
 
--- Rank the origins based on the number of fans
-SELECT origin, nb_fans,
-       RANK() OVER (ORDER BY nb_fans DESC) AS country_rank
-FROM temp_rankings
-ORDER BY country_rank;
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands 
+GROUP BY origin
+ORDER BY nb_fans DESC;
